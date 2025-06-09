dfffdb9b6e76efc802e64d066f584315ec6c6f46
Jinho Yun <owlflyx12@gmail.com>
2025-06-09 21:50:11 +0900

    feat: allow array of paths for fs-mkdir and fs-touch tasks

--- src/tasks/handleFsMkdir.ts
+++ src/tasks/handleFsMkdir.ts
@@ -5,12 +5,18 @@ import { checkTypeOrThrow } from '@/utils';
 import { z } from 'zod';
 
 export const TaskFsMakeDirSchema = newTaskSchema('fs-mkdir', {
-  path: z.string().nonempty(),
+  path: z.union([z.string().nonempty(), z.array(z.string().nonempty())]),
 });
 export type TaskFsMakeDir = z.infer<typeof TaskFsMakeDirSchema>;
 
 export const handleMkdir = async (context: TaskContext, task: TaskFsMakeDir) => {
-  checkTypeOrThrow('path', task.path, ['string']);
-  logv(`Make a directory at : ${task.path}`);
-  mkdirpSync(task.path);
+  if (Array.isArray(task.path)) {
+    for (const p of task.path) {
+      logv(`Make a directory at : ${p}`);
+      mkdirpSync(p);
+    }
+  } else {
+    logv(`Make a directory at : ${task.path}`);
+    mkdirpSync(task.path);
+  }
 };
--- src/tasks/handleFsTouch.ts
+++ src/tasks/handleFsTouch.ts
@@ -4,12 +4,20 @@ import fse from 'fs-extra';
 import { z } from 'zod';
 
 export const TaskFsTouchSchema = newTaskSchema('fs-touch', {
-  path: z.string().nonempty(),
+  path: z.union([z.string().nonempty(), z.array(z.string().nonempty())]),
 });
 export type TaskFsTouch = z.infer<typeof TaskFsTouchSchema>;
 
 export const handleFsTouch = async (context: TaskContext, task: TaskFsTouch) => {
-  logv(`Touch a file at : ${task.path}`);
-  if (fse.existsSync(task.path)) return;
-  fse.writeFileSync(task.path, '');
+  if (Array.isArray(task.path)) {
+    for (const p of task.path) {
+      if (fse.existsSync(p)) continue;
+      logv(`Touch a file at : ${p}`);
+      fse.writeFileSync(p, '');
+    }
+  } else {
+    if (fse.existsSync(task.path)) return;
+    logv(`Touch a file at : ${task.path}`);
+    fse.writeFileSync(task.path, '');
+  }
 };
